81b239efdda33b031977eca6514efa4b2d172c87
sagar <sagarsonkar2303@gmail.com>
2022-08-05 23:43:29 +0530

    logger

---
+++ src/logger/logger.js
@@ -0,0 +1,19 @@
+// const myName1= "Sagar"
+
+// let realName = function(){
+//     console.log("Welcome to my application I am",myName1 + " " + "and i am part of FUNCTIONUP Plutonium cohort")
+
+// }
+
+// module.exports.fun1=myName1
+// module.exports.realName=realName
+
+const myName2= "Sagar"
+
+let Welcome = function(){
+    console.log("Welcome to my application I am",myName2 + " " + "and i am part of FUNCTIONUP Plutonium cohort")
+    
+}
+
+module.exports.fun=myName2
+module.exports.Welcome =Welcome
