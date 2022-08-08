7dd35dfc0361fc77794e1aef5140ea9a10f2fea8
sagar <sagarsonkar2303@gmail.com>
2022-08-08 18:07:50 +0530

    updated lodesh

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const lodash = require('lodash');
 const abc = require('../introduction/intro')
 const xyz =require('../logger/logger')
 const info =require('../util/helper')
@@ -11,6 +12,27 @@ router.get('/test-me', function (req, res) {
     xyz.Welcome()
     info.today()
     formatter.form()
+
+const arrayOfMonths= ['jan','feb','mar','april','may','june','july','aug','sept','oct','nov','dec']
+ console.log (lodash.chunk(arrayOfMonths,4));
+
+ const oddNum=[1,3,5,7,11,13,17,19,23,29]
+ console.log(lodash.tail(oddNum));
+
+const num1= [1,2,3,4]
+const num2=[2,3,4,5]
+const num3=[3,4,5,6]
+const num4=[6,7,8,9]
+const num5=[7,8,9,10]
+console.log(lodash.union(num1,num2,num3,num4,num5));
+
+
+const moVies=[["horror","The Shining"],["drama","Titanic"],["thriller","Shutter Island"],["fantasy","Pans Labyrinth"]]
+let obj =lodash.fromPairs(moVies)
+console.log(obj);
+
+
+
     res.send('My second ever api!')
 });
 
