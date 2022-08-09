922050446210384152fbae23644fec9ea0c9e4ca
sagar <sagarsonkar2303@gmail.com>
2022-08-09 10:39:56 +0530

    assignment done

--- src/routes/route.js
+++ src/routes/route.js
@@ -12,9 +12,85 @@ router.get('/test-me', function (req, res) {
 
 router.get('/students', function (req, res){
     let students = ['Sabiha', 'Neha', 'Akash']
-    res.send(students)
+    res.send(students)   
 })
 
+//problem 1
+router.get('/movies/', function (req,res){
+    let movies  = ["Rang de basanti", "The shining", "Lord of the rings", "Batman begins"]
+    res.send(movies)
+});
+
+
+//problem 2
+
+router.get('/movies/:indexNumber',function (req, res){
+    let moviesName = ["Rang de basanti", "The shining", "Lord of the rings", "Batman begins"]
+    let abc = req.params.indexNumber
+    console.log(moviesName[abc])
+    res.send(moviesName[abc])
+});
+
+
+//problem 3
+
+router.get('/my/movies/:indexNumber', function (req, res){
+    const movies = ["Tenet","The Btamna Begins","Pather Pachali","Charlie The Choclate Factory"];
+    const index = req.params.indexNumber;
+       if(index<4){
+        res.send(movies[index]);
+        console.log(movies[index]);
+       }else{
+        res.send("use a valid index");
+        console.log("use a valid index");
+       }
+});
+
+
+//problem 4
+
+router.get('/hit/films', function (req, res){
+    const films = [{
+     "id": 1,
+     "name": "The Shining"
+    }, {
+     "id": 2,
+     "name": "Incendies"
+    }, {
+     "id": 3,
+     "name": "Rang de Basanti"
+    }, {
+     "id": 4,
+    "name": "Finding Nemo"
+    }]
+    res.send(films);
+});
+
+//problem 5
+router.get('/films/:filmId', function(req,res){
+    const films = [{
+        "id": 1,
+        "name": "The Shining"
+       }, {
+        "id": 2,
+        "name": "Incendies"
+       }, {
+        "id": 3,
+        "name": "Rang de Basanti"
+       }, {
+        "id": 4,
+       "name": "Finding Nemo"
+       }]
+       const xyz = req.params.filmId;
+       if(xyz<5 && xyz > 0){
+        res.send(films[xyz-1]);
+        console.log((films[xyz-1]))
+       }else{
+        res.send("No movie exists with this id")
+        console.log("No movie exists with this id")
+       }
+
+   });
 router.get('/student-details/:name', function(req, res){
     /*
     params is an attribute inside request that contains 
